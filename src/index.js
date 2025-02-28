da16413a6d78f12131b0abec1ea3f1bec6c91a99
DanielaRochaMunoz <A00573664@tec.mx>
2025-02-27 23:02:37 -0600

    Tarea

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-function calculate() {
-  const num1 = document.getElementById( "num1").value; 
-  const num2 = document.getElementById ("num2").value; 
-  
-  let result; 
-  result = Number(num1) + Number(num2); 
-  
-  document.getElementById("result").innerText = "Result: " + result; 
-} 
+function addition() {
+  const num1 = document.getElementById("num1").value;
+  const num2 = document.getElementById("num2").value;
+
+  let result;
+  result = Number(num1) + Number(num2);
+
+  document.getElementById("result").innerText = "Result: " + result;
+  document.getElementById("operator").innerText = "+"; // Cambio de signo
+}
+
+function subtraction() {
+  const num1 = document.getElementById("num1").value;
+  const num2 = document.getElementById("num2").value;
+
+  let result;
+  result = Number(num1) - Number(num2);
+
+  document.getElementById("result").innerText = "Result: " + result;
+  document.getElementById("operator").innerText = "−"; // Cambio de signo
+}
 
 function resetFields() {
-  document.getElementById ("num1").value = ""; 
-  document.getElementById("num2").value = ""; 
-  document.getElementById ("operator").value = "+"; 
-  document.getElementById ("result").innerText = "Result: ";
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
+  document.getElementById("operator").innerText = "+"; // Reinicia el signo
+  document.getElementById("result").innerText = "Result: ";
 }
+
